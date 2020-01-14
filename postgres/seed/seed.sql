d37de91496892c0a0dd60563e3bab51ec377eac8
Derek Adams <derekadams.uas@gmail.com>
2020-01-13 19:11:37 -0700

    added seed data for postgres

---
+++ postgres/seed/seed.sql
@@ -0,0 +1,13 @@
+BEGIN TRANSACTION;
+
+INSERT INTO users
+    (name, email, entries, joined)
+VALUES
+    ('Jessie', 'jessie@gmail.com', 5, '2018-01-01')
+
+INSERT INTO login
+    (hash, email)
+VALUES
+    ('hash', 'jessie@gmail.com')
+
+COMMIT;
