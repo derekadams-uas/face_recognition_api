3de0300d899633b8c9326549eeaa6c2b69524040
Derek Adams <derekadams.uas@gmail.com>
2020-01-19 09:31:47 -0700

    added handleProfileUpdate

--- controllers/profile.js
+++ controllers/profile.js
@@ -11,6 +11,23 @@ const handleProfileGet = (req, res, db) => {
         .catch(err => res.status(400).json('Error Getting User'));
 }
 
+const handleProfileUpdate = (req, res, db) => {
+    const { id } = req.params;
+    const { name, age, pet } = req.body.formInput;
+    db('users')
+        .where({ id: id })
+        .update({ name: name })
+        .then(resp => {
+            if (resp) {
+                res.json("Success");
+            } else {
+                res.status(400).json("Unable to Update");
+            }
+        })
+        .catch(err => res.status(400).json("Error Updating User"));
+}
+
 module.exports = {
-    handleProfileGet
+    handleProfileGet,
+    handleProfileUpdate
 }
